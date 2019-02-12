4a1a632a980e56413ab497f0c599e40197cdfb5b
Rubens Mariuzzo <rubens@mariuzzo.com>
2019-02-12 01:10:20 -0400

    Add NotFound error.

--- src/error/RepositoryError.ts
+++ src/error/RepositoryError.ts
@@ -1,5 +1,14 @@
 export default class RepositoryError extends Error {
-  constructor(...args) {
-    super(...args)
+  static NotFound = class extends RepositoryError {
+    constructor(message: string) {
+      super(message, 404)
+    }
+  }
+  
+  status: number
+
+  constructor(message: string, status: number) {
+    super(message)
+    this.status = status
   }
 }
